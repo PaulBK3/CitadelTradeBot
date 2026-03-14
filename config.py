4d189c36cd485b703e221f7c42498fd6db5ce2df
PaulBK3 <paul.schumacher97@gmx.de>
2026-03-14 18:09:09 +0100

    added regions

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TRADE_CHARTA_ROLE = "Trade Charta"
+TRADE_CHARTER_ROLE = "Charter"
 TRADE_TEAM_ROLE = "Travel Team"
 
 TRADE_LOG_CHANNEL = "trade-log"
@@ -16,175 +16,296 @@ REGION_ROLES = [
     "Dorne",
     "Iron Islands",
     "Crownlands",
-    "Essos"
+    "Dragonstone",
+    "Andalos",
+    "Braavos",
+    "Disputed Lands",
+    "Lys",
+    "Myr",
+    "Pentos",
+    "Tyrosh"
 ]
 
 # Weekly production per region
 PRODUCTION = {
 
     "North": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
-        "Iron": 3,
-        "Luxury": 3,
-        "Food": 3,
+        "Food": 30,
+        "Wood": 33,
+        "Stone": 6,
+        "Iron": 5,
+        "Luxury": 4
     },
 
     "Vale": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
-        "Iron": 3,
-        "Luxury": 3,
-        "Food": 3,
+        "Food": 19,
+        "Wood": 12,
+        "Stone": 15,
+        "Iron": 8,
+        "Luxury": 2
     },
 
     "Riverlands": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
-        "Iron": 3,
+        "Food": 37,
+        "Wood": 13,
+        "Stone": 5,
+        "Iron": 1,
         "Luxury": 3,
-        "Food": 3,
     },
 
     "Westerlands": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
-        "Iron": 3,
-        "Luxury": 3,
-        "Food": 3,
+        "Food": 16,
+        "Wood": 13,
+        "Stone": 15,
+        "Iron": 4,
+        "Luxury": 2
     },
 
     "Reach": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
-        "Iron": 3,
-        "Luxury": 3,
-        "Food": 3,
+        "Food": 80,
+        "Wood": 17,
+        "Stone": 6,
+        "Iron": 2,
+        "Luxury": 5,
     },
 
     "Stormlands": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
+        "Food": 17,
+        "Wood": 23,
+        "Stone": 9,
         "Iron": 3,
-        "Luxury": 3,
-        "Food": 3,
+        "Luxury": 5,
     },
 
     "Dorne": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
-        "Iron": 3,
-        "Luxury": 3,
-        "Food": 3,
+        "Food": 17,
+        "Wood": 5,
+        "Stone": 17,
+        "Iron": 8,
+        "Luxury": 7,
     },
 
     "Iron Islands": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
-        "Iron": 3,
-        "Luxury": 3,
-        "Food": 3,
+        "Food": 7,
+        "Wood": 0,
+        "Stone": 5,
+        "Iron": 15,
+        "Luxury": 0,
     },
 
     "Crownlands": {
-        "Food": 3,
-        "Wood": 1,
+        "Food": 13,
+        "Wood": 12,
         "Stone": 3,
+        "Iron": 1,
+        "Luxury": 4,
+    },
+
+    "Dragonstone":{
+        "Food": 6,
+        "Wood": 1,
+        "Stone": 5,
+        "Iron": 1,
+        "Luxury": 1,
+    },
+
+    "Andalos": {
+        "Food": 0,
+        "Wood": 2,
+        "Stone": 7,
         "Iron": 3,
-        "Luxury": 3,
+        "Luxury": 0,
+    },
+
+    "Braavos": {
+        "Food": 13,
+        "Wood": 4,
+        "Stone": 1,
+        "Iron": 0,
+        "Luxury": 19,
+    },
+
+    # These reprisent reasorses to be gained via conqering. but i'll add them anyway
+    "Disputed Lands": {
+        "Food": 1,
+        "Wood": 5,
+        "Stone": 3,
+        "Iron": 1,
+        "Luxury": 0,
+    },
+
+    "Lys": {
+        "Food": 2,
+        "Wood": 10,
+        "Stone": 0,
+        "Iron": 0,
+        "Luxury": 11,
+    },
+
+    "Myr": {
         "Food": 3,
-    }
+        "Wood": 2,
+        "Stone": 0,
+        "Iron": 0,
+        "Luxury": 14,
+    },
+
+    "Pentos": {
+        "Food": 6,
+        "Wood": 2,
+        "Stone": 0,
+        "Iron": 0,
+        "Luxury": 19,
+    },
+
+    "Tyrosh": {
+        "Food": 3,
+        "Wood": 3,
+        "Stone": 0,
+        "Iron": 0,
+        "Luxury": 12,
+    },
+
 }
 
 # Weekly maintenance per region
 MAINTENANCE = {
 
     "North": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
-        "Iron": 3,
-        "Luxury": 3,
-        "Food": 3,
+        "Food": 30,
+        "Wood": 22,
+        "Stone": 5,
+        "Iron": 5,
+        "Luxury": 6,
     },
 
     "Vale": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
-        "Iron": 3,
-        "Luxury": 3,
-        "Food": 3,
+        "Food": 20,
+        "Wood": 14,
+        "Stone": 5,
+        "Iron": 2,
+        "Luxury": 5,
     },
 
     "Riverlands": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
-        "Iron": 3,
+        "Food": 25,
+        "Wood": 14,
+        "Stone": 7,
+        "Iron": 2,
         "Luxury": 3,
-        "Food": 3,
     },
 
     "Westerlands": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
-        "Iron": 3,
+        "Food": 17,
+        "Wood": 13,
+        "Stone": 5,
+        "Iron": 0,
         "Luxury": 3,
-        "Food": 3,
     },
 
     "Reach": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
+        "Food": 62,
+        "Wood": 20,
+        "Stone": 8,
         "Iron": 3,
-        "Luxury": 3,
-        "Food": 3,
+        "Luxury": 6,
     },
 
     "Stormlands": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
-        "Iron": 3,
-        "Luxury": 3,
-        "Food": 3,
+        "Food": 18,
+        "Wood": 12,
+        "Stone": 9,
+        "Iron": 4,
+        "Luxury": 6,
     },
 
     "Dorne": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
-        "Iron": 3,
-        "Luxury": 3,
-        "Food": 3,
+        "Food": 19,
+        "Wood": 6,
+        "Stone": 11,
+        "Iron": 5,
+        "Luxury": 6,
     },
 
     "Iron Islands": {
-        "Food": 3,
-        "Wood": 1,
-        "Stone": 3,
-        "Iron": 3,
-        "Luxury": 3,
-        "Food": 3,
+        "Food": 9,
+        "Wood": 2,
+        "Stone": 5,
+        "Iron": 2,
+        "Luxury": 0,
     },
 
     "Crownlands": {
-        "Food": 3,
+        "Food": 15,
+        "Wood": 12,
+        "Stone": 3,
+        "Iron": 1,
+        "Luxury": 6,
+    },
+
+    "Dragonstone":{
+        "Food": 2,
+        "Wood": 2,
+        "Stone": 1,
+        "Iron": 0,
+        "Luxury": 2,
+    },
+
+    "Andalos": {
+        "Food": 1,
         "Wood": 1,
         "Stone": 3,
-        "Iron": 3,
-        "Luxury": 3,
+        "Iron": 1,
+        "Luxury": 1,
+    },
+
+    "Braavos": {
+        "Food": 12,
+        "Wood": 7,
+        "Stone": 1,
+        "Iron": 1,
+        "Luxury": 4,
+    },
+
+    # See message in production for the reason you see what you see bellow
+    "Disputed Lands": {
+        "Food": 0,
+        "Wood": 0,
+        "Stone": 0,
+        "Iron": 0,
+        "Luxury": 0,
+    },
+
+    "Lys": {
+        "Food": 4,
+        "Wood": 2,
+        "Stone": 1,
+        "Iron": 2,
+        "Luxury": 4,
+    },
+    
+    "Myr": {
         "Food": 3,
-    }
+        "Wood": 2,
+        "Stone": 1,
+        "Iron": 2,
+        "Luxury": 4,
+    },
+
+    "Pentos": {
+        "Food": 6,
+        "Wood": 4,
+        "Stone": 2,
+        "Iron": 1,
+        "Luxury": 4,
+    },
+
+    "Tyrosh": {
+        "Food": 3,
+        "Wood": 2,
+        "Stone": 2,
+        "Iron": 0,
+        "Luxury": 4,
+    },
+
 }

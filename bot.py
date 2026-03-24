c8d8cff8580a36d3108bac32f707d22040cd8ad1
PaulBK3 <paul.schumacher97@gmx.de>
2026-03-24 16:09:02 +0100

    refactor trade confirmation message for clarity in resource transfer

--- bot.py
+++ bot.py
@@ -427,9 +427,7 @@ class TradeConfirm(discord.ui.View):
                 else:
                     comment_text = ""
                 await save_edit.send(
-                    f"{self.sender}:\n- Remove {self.amount} gold "
-                    f"{self.receiver}:\n- Add {self.amount} gold "
-                    f"(Trade #{trade_id}){comment_text}"
+                    f"- Transfer {self.amount} gold from {self.sender} to {self.receiver} (Trade #{trade_id}){comment_text}"
                 )
 
         msg = (

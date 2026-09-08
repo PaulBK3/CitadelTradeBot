190083c6016accbd5ae649e9ffad87f739f6ca0a
PaulBK3 <paul.schumacher97@gmx.de>
2026-09-08 23:08:35 +0200

    .

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 intents = discord.Intents.default()
+intents.members = True 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 database.setup()
